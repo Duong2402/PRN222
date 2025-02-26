b025fdc9a5b1271f5d812191e9e5a7ebec9692f4
ducbinh <binhcdhe172265@fpt.edu.vn>
2025-02-26 10:56:05 +0700

    Profile User

--- OnlineMusicProject/Controllers/UsersController.cs
+++ OnlineMusicProject/Controllers/UsersController.cs
@@ -1,9 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineMusicProject.Models;
+using OnlineMusicProject.ViewModels;
 
 namespace OnlineMusicProject.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly UserManager<Users> userManager;
+
+        public UsersController(UserManager<Users> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Profile(Users u)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+                if(user != null)
+                {
+                    user.FullName = u.FullName;
+                    var result = await userManager.UpdateAsync(user);
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Profile");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(u);
+        }
         public IActionResult AlbumsStore()
         {
             return View();
--- OnlineMusicProject/obj/Debug/net8.0/OnlineMusicProject.AssemblyInfo.cs
+++ OnlineMusicProject/obj/Debug/net8.0/OnlineMusicProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineMusicProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64ec52bb39ddeab79d112110c62d66131aad76a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1170ee25d78f6da6a28e47851f2da9b7980dcee")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineMusicProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineMusicProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
