4cdd5fddbfe7809a10619709430275877f4754df
ducbinh <binhcdhe172265@fpt.edu.vn>
2025-02-26 10:54:09 +0700

    Profile User

--- OnlineMusicProject/Models/Users.cs
+++ OnlineMusicProject/Models/Users.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace OnlineMusicProject.Models
 {
     public class Users: IdentityUser
     {
+        [Display(Name = "Full Name")]
+        [StringLength(50, MinimumLength = 1)]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "FullName can only contain letters and spaces.")]
         public string FullName { get; set; }
 
     }
